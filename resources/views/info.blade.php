95682e87708c99d10f0b0a767f5b993f7c05ae7e
Romans M <romanmay890@gmail.com>
2026-03-20 13:09:30 +0200

    added new Route '/info' and new view 'info' with lorem25

---
+++ resources/views/info.blade.php
@@ -0,0 +1,3 @@
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente mollitia adipisci fugiat saepe unde! Qui optio corporis rem ipsa libero voluptas nemo aperiam ducimus ex.
+</p>
